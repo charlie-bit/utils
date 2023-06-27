4ac6fcfceeaec2b3dfdfdbd3d7a4ef24cd752158
shengnan <shengnan@89trillion.com>
2023-06-27 17:18:14 +0800

    信号监听相关

---
+++ system/signals.go
@@ -0,0 +1,36 @@
+package system
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func init() {
+	go func() {
+		// https://golang.org/pkg/os/signal/#Notify
+		signals := make(chan os.Signal, 1)
+		signal.Notify(signals, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGTERM)
+
+		for {
+			v := <-signals
+			switch v {
+			case syscall.SIGUSR1:
+				log.Println("syscall.SIGUSR1")
+			case syscall.SIGUSR2:
+				log.Println("syscall.SIGUSR1")
+			case syscall.SIGTERM:
+				gracefulStop(signals)
+			default:
+				log.Println("Got unregistered signal:", v)
+			}
+		}
+	}()
+}
+
+func gracefulStop(signals chan os.Signal) {
+	signal.Stop(signals)
+	log.Println("Got signal SIGTERM, shutting down...")
+	syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+}
